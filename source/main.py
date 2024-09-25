60076c4b760c37a3d9fcc4a7ac75f9e2962d25c2
Eva Brátová <bratovae23@spstrutnov.cz>
2024-09-25 19:10:33 +0200

    class Hráč

--- source/main.py
+++ source/main.py
@@ -6,30 +6,42 @@ pygame.display.set_caption("PyFarm")
 screen_size = 1000, 600
 screen = pygame.display.set_mode(screen_size)
 
+class Hrac(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = postava1 = pygame.image.load("images/postava1.png").convert_alpha()
+        self.rect = postava1.get_rect(center = (500,300)) 
+        self.speed = 6
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+    
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed  
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed 
+        if keys[pygame.K_UP]:
+            self.rect.y -= self.speed 
+        if keys[pygame.K_DOWN]:
+            self.rect.y += self.speed 
+    def update(self):
+        self.input()
+
+hrac = Hrac()
+
+mapa1 = pygame.image.load("images/mapa1.png")
+
+
 running = True
 
-postava1 = pygame.image.load("images/postava1.png")
-postava_rect = postava1.get_rect(center = (500,300)) 
-speed = 6
-
 while running:
+    
     screen.fill((255,255,255))
-    screen.blit(postava1,postava_rect)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_LEFT]:
-        postava_rect.x -= speed  
-    if keys[pygame.K_RIGHT]:
-        postava_rect.x += speed  
-    if keys[pygame.K_UP]:
-        postava_rect.y -= speed 
-    if keys[pygame.K_DOWN]:
-        postava_rect.y += speed  
-
+    hrac.update()
+    screen.blit(hrac.image, hrac.rect)
     
     pygame.display.update()
     pygame.time.Clock().tick(60)
